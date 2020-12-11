0c4de938b8273d63ac87adac7cea0bb21e539df9
Matt DeVillier <matt.devillier@puri.sm>
2020-12-11 16:18:13 -0600

    build/build_release.sh: Add build scripts
    Add scripts to build all Librem boards, build a full release.
    
    Signed-off-by: Matt DeVillier <matt.devillier@puri.sm>

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+first() {
+	echo "$1"
+}
+
+all_boards=('librem_13v2' 'librem_15v3' \
+			'librem_13v4' 'librem_15v4' \
+			'librem_mini' 'librem_mini_v2' \
+			'librem_14' "librem_l1um")
+
+if [ -z "$1" ]; then
+	build_targets=("${all_boards[@]}");
+else
+	build_targets=("$@")
+fi
+
+for board in "${build_targets[@]}"
+do
+	make BOARD=$board
+	
+	# If any preconfigurations exist for this board, create a ROM for each
+	for config in "preconfigure/$board"/*; do
+		if ! [ -f "$config" ]; then
+			continue;
+		fi
+		
+		config_name="$(basename "$config")"
+		rom_path="build/x86/$board"
+		rom_version="$(git describe --abbrev=7 --tags --dirty)"
+		base_rom_name="pureboot-$board-$rom_version.rom"
+		config_rom_name="pureboot-$board-$config_name-$rom_version.rom"
+		cbfstool="$(first build/x86/coreboot-*/"$board"/cbfstool)"
+		
+		cp "$rom_path/$base_rom_name" "$rom_path/$config_rom_name"
+		"$cbfstool" "$rom_path/$config_rom_name" add -n heads/initrd/etc/config.user -f "$config" -t raw
+		echo "Built preconfigured ROM $rom_path/$config_rom_name"
+	done
+done
