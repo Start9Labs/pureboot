b8b0d4b3a3397f48167e83b3f0d994106105435c
Kyle Rankin <kyle.rankin@puri.sm>
2021-09-09 10:24:42 -0700

    Add hash file detection and initialization menu
    To make this a bit easier to use, I added a feature to detect whether
    the hash file exists and if not, to display a more limited menu to the
    user guiding them to create the initial hash file. Otherwise it will
    display the date the file was last modified, which can be useful to
    determine how stale it is.

--- initrd/bin/root-hashes-gui.sh
+++ initrd/bin/root-hashes-gui.sh
@@ -144,12 +144,28 @@ unmount_root_device()
 
 while true; do
   unset menu_choice
-  whiptail --clear --title "Root Disk Verification Menu" \
-    --menu "This feature lets you detect tampering in files on your root disk\n\nYou can initialize, update, and check hashes for files in\n $CONFIG_ROOT_DIRLIST\n\nSelect the root hash function to perform" 20 90 10 \
-    'c' ' Check root hashes' \
-    'u' ' Initialize/Update root hashes' \
-    'x' ' Exit' \
-    2>/tmp/whiptail || recovery "GUI menu failed"
+
+  # mount /boot RO to detect hash file
+  if ! grep -q /boot /proc/mounts ; then
+    mount -o ro /boot \
+    || $(unmount_root_device && die "Unable to mount /boot")
+  fi
+
+  if [ -e "$HASH_FILE" ]; then
+    HASH_FILE_DATE=$(stat -c %y ${HASH_FILE})
+    whiptail --clear --title "Root Disk Verification Menu" \
+      --menu "This feature lets you detect tampering in files on your root disk.\n\nHash file last updated: ${HASH_FILE_DATE}\n\nYou can check and update hashes for files in:\n $CONFIG_ROOT_DIRLIST\n\nSelect the function to perform:" 20 90 10 \
+      'c' ' Check root hashes' \
+      'u' ' Update root hashes' \
+      'x' ' Exit' \
+      2>/tmp/whiptail || recovery "GUI menu failed"
+  else
+    whiptail --clear --title "Root Disk Verification Menu" \
+      --menu "This feature lets you detect tampering in files on your root disk.\n\nNo hash file has been created yet\n\nYou can create hashes for files in:\n $CONFIG_ROOT_DIRLIST\n\nSelect the function to perform:" 20 90 10 \
+      'u' ' Create root hashes' \
+      'x' ' Exit' \
+      2>/tmp/whiptail || recovery "GUI menu failed"
+  fi
 
   menu_choice=$(cat /tmp/whiptail)
 
