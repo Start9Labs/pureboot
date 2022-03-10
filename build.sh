fb7c9912f6a98359691d04c4fdb741ce07733b1e
Matt DeVillier <matt.devillier@puri.sm>
2022-03-09 18:07:28 -0600

    WIP: add support for AX200 wifi/BT
    Signed-off-by: Matt DeVillier <matt.devillier@puri.sm>

--- build.sh
+++ build.sh
@@ -22,11 +22,21 @@ do
 		-n firmware/iwlwifi-9260-th-b0-jf-b0-46.ucode.lzma \
 		-f blobs/librem_jail/iwlwifi-9260-th-b0-jf-b0-46.ucode
 	cbfstool build/$board/pureboot-$board-$GIT_VERSION.rom add -t raw -c lzma \
+		-n firmware/iwlwifi-cc-a0-59.ucode.lzma \
+		-f blobs/librem_jail/iwlwifi-cc-a0-59.ucode
+#	cbfstool build/$board/pureboot-$board-$GIT_VERSION.rom add -t raw -c lzma \
+#		-n firmware/iwlwifi-cc-a0-63.ucode.lzma \
+#		-f blobs/librem_jail/iwlwifi-cc-a0-63.ucode
+	cbfstool build/$board/pureboot-$board-$GIT_VERSION.rom add -t raw -c lzma \
 		-n firmware/iwlwifi-ty-a0-gf-a0-63.ucode.lzma \
 		-f blobs/librem_jail/iwlwifi-ty-a0-gf-a0-63.ucode
 	cbfstool build/$board/pureboot-$board-$GIT_VERSION.rom add -t raw -c lzma \
 		-n firmware/iwlwifi-ty-a0-gf-a0.pnvm.lzma \
 		-f blobs/librem_jail/iwlwifi-ty-a0-gf-a0.pnvm
+	cbfstool build/$board/pureboot-$board-$GIT_VERSION.rom add -t raw \
+		-n firmware/intel/ibt-20-1-3.ddc -f blobs/librem_jail/intel/ibt-20-1-3.ddc
+	cbfstool build/$board/pureboot-$board-$GIT_VERSION.rom add -t raw -c lzma \
+		-n firmware/intel/ibt-20-1-3.sfi.lzma -f blobs/librem_jail/intel/ibt-20-1-3.sfi 
 	cbfstool build/$board/pureboot-$board-$GIT_VERSION.rom add -t raw \
 		-n firmware/intel/ibt-18-16-1.ddc -f blobs/librem_jail/intel/ibt-18-16-1.ddc
 	cbfstool build/$board/pureboot-$board-$GIT_VERSION.rom add -t raw -c lzma \
