c23d0ee20a342f19b878c5ee33e03d9dedd211f0
Matt DeVillier <matt.devillier@puri.sm>
2022-03-15 12:05:04 -0500

    Add Pureboot Basic Mode
    PureBoot Basic mode provides the full Linux userspace in firmware from
    Heads without requiring verified boot or a Librem Key.  Basic and
    verified boot can be switched freely without changing firmware, such as
    if a Librem Key is lost.
    
    PureBoot Basic can apply firmware updates from a USB flash drive, and
    having a complete Linux userspace enables more sophisticated recovery
    options.
    
    Signed-off-by: Matt DeVillier <matt.devillier@puri.sm>

--- initrd/bin/config-gui.sh
+++ initrd/bin/config-gui.sh
@@ -15,6 +15,15 @@ while true; do
     menu_choice=${param::1}
     unset param
   else
+    # check current Pureboot Mode
+    if grep -q 'CONFIG_PUREBOOT_BASIC' /tmp/config; then 
+      BASIC_MODE=`grep 'CONFIG_PUREBOOT_BASIC=' /tmp/config | tail -n1 | cut -f2 -d '=' | tr -d '"'`
+      [ "$BASIC_MODE" == "y" ] && MODE_ACTION="Disable" || MODE_ACTION="Enable"
+    else
+      BASIC_MODE=n
+      MODE_ACTION="Enable"
+    fi
+
     unset menu_choice
     whiptail $BG_COLOR_MAIN_MENU --clear --title "Config Management Menu" \
     --menu "This menu lets you change settings for the current BIOS session.\n\nAll changes will revert after a reboot,\n\nunless you also save them to the running BIOS." 20 90 10 \
@@ -23,6 +32,7 @@ while true; do
     'R' ' Change the root device for hashing' \
     'D' ' Change the root directories to hash' \
     'B' ' Check root hashes at boot' \
+    'P' " $MODE_ACTION Pureboot Basic Mode" \
     's' ' Save the current configuration to the running BIOS' \
     'x' ' Return to Main Menu' \
     2>/tmp/whiptail || recovery "GUI menu failed"
@@ -227,6 +237,42 @@ while true; do
 
           whiptail --title 'Config change successful' \
             --msgbox "The root device will not be checked at each boot." 16 60
+        fi
+      fi
+    ;;
+    "P" )
+      if [ "$BASIC_MODE" = "n" ]; then
+        if (whiptail --title 'Enable Pureboot Basic Mode?' \
+             --yesno "This will remove all signature checking on the firmware
+                    \nand boot files, and disable use of the Librem Key.
+                    \n\nDo you want to proceed?" 16 90) then
+
+          if grep -q "CONFIG_PUREBOOT_BASIC" /etc/config.user; then
+            replace_config /etc/config.user "CONFIG_PUREBOOT_BASIC" "y"
+          else
+            echo "export CONFIG_PUREBOOT_BASIC=y" >> /etc/config.user
+          fi
+          combine_configs
+
+          whiptail --title 'Config change successful' \
+            --msgbox "Pureboot Basic mode enabled;\nsave the config change and reboot for it to go into effect." 16 60
+
+        fi
+      else
+        if (whiptail --title 'Disable Pureboot Basic Mode?' \
+             --yesno "This will enable all signature checking on the firmware
+                    \nand boot files, and enable use of the Librem Key.
+                    \n\nDo you want to proceed?" 16 90) then
+
+          if grep -q "CONFIG_PUREBOOT_BASIC" /etc/config.user; then
+            replace_config /etc/config.user "CONFIG_PUREBOOT_BASIC" "n"
+          else
+            echo "export CONFIG_PUREBOOT_BASIC=n" >> /etc/config.user
+          fi
+          combine_configs
+
+          whiptail --title 'Config change successful' \
+            --msgbox "Pureboot Basic mode has been disabled;\nsave the config change and reboot for it to go into effect." 16 60
         fi
       fi
     ;;
