449eca7b72b042c97c3894043d5c4f8e41d51d45
Matt DeVillier <matt.devillier@puri.sm>
2022-03-15 12:07:10 -0500

    config_gui: Add toggle for Pureboot Basic mode
    Signed-off-by: Matt DeVillier <matt.devillier@puri.sm>

--- initrd/bin/config-gui.sh
+++ initrd/bin/config-gui.sh
@@ -23,6 +23,14 @@ while true; do
       USE_JAIL=n
       JAIL_ACTION="Enable"
     fi
+    # check current Pureboot Mode
+    if grep -q 'CONFIG_PUREBOOT_BASIC' /tmp/config; then 
+      BASIC_MODE=`grep 'CONFIG_PUREBOOT_BASIC=' /tmp/config | tail -n1 | cut -f2 -d '=' | tr -d '"'`
+      [ "$BASIC_MODE" == "y" ] && MODE_ACTION="Disable" || MODE_ACTION="Enable"
+    else
+      BASIC_MODE=n
+      MODE_ACTION="Enable"
+    fi
 
     unset menu_choice
     whiptail $BG_COLOR_MAIN_MENU --clear --title "Config Management Menu" \
@@ -33,6 +41,7 @@ while true; do
     'D' ' Change the root directories to hash' \
     'B' ' Check root hashes at boot' \
     'J' " $JAIL_ACTION Firmware Blob Jail" \
+    'P' " $MODE_ACTION Pureboot Basic Mode" \
     's' ' Save the current configuration to the running BIOS' \
     'x' ' Return to Main Menu' \
     2>/tmp/whiptail || recovery "GUI menu failed"
@@ -273,6 +282,42 @@ while true; do
 
           whiptail --title 'Config change successful' \
             --msgbox "Firmware Blob Jail use has been disabled;\nsave the config change and reboot for it to go into effect." 16 60
+        fi
+      fi
+    ;;
+    "P" )
+      if [ "$BASIC_MODE" = "n" ]; then
+        if (whiptail --title 'Enable Pureboot Basic Mode?' \
+             --yesno "This will remove all signature checking on the firmware
+                    \nand boot files, and disable use of the Librem Key.
+                    \n\nDo you want to proceed?" 16 90) then
+
+          if grep -q "CONFIG_PUREBOOT_BASIC" /etc/config.user; then
+            replace_config /etc/config.user "CONFIG_PUREBOOT_BASIC" "y"
+          else
+            echo "export CONFIG_PUREBOOT_BASIC=y" >> /etc/config.user
+          fi
+          combine_configs
+
+          whiptail --title 'Config change successful' \
+            --msgbox "Pureboot Basic mode enabled;\nsave the config change and reboot for it to go into effect." 16 60
+
+        fi
+      else
+        if (whiptail --title 'Disable Pureboot Basic Mode?' \
+             --yesno "This will enable all signature checking on the firmware
+                    \nand boot files, and enable use of the Librem Key.
+                    \n\nDo you want to proceed?" 16 90) then
+
+          if grep -q "CONFIG_PUREBOOT_BASIC" /etc/config.user; then
+            replace_config /etc/config.user "CONFIG_PUREBOOT_BASIC" "n"
+          else
+            echo "export CONFIG_PUREBOOT_BASIC=n" >> /etc/config.user
+          fi
+          combine_configs
+
+          whiptail --title 'Config change successful' \
+            --msgbox "Pureboot Basic mode has been disabled;\nsave the config change and reboot for it to go into effect." 16 60
         fi
       fi
     ;;
